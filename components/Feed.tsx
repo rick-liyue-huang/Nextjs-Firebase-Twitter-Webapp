24bf2b35a96f1da2b49f81626803852731126d73
rick-liyue-huang <rick.liyue.huang@gmail.com>
2022-08-15 14:53:02 +1000

    feat: complete feed part design

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,8 +1,30 @@
 import { SparklesIcon } from '@heroicons/react/outline';
 import React from 'react';
 import { InputComponent } from './Input';
+import { PostComponent } from './Post';
 
 export const FeedComponent: React.FC = () => {
+  const posts = [
+    {
+      id: '1',
+      name: 'post one',
+      username: 'user one',
+      userImg: 'https://avatars.githubusercontent.com/u/20208332?s=40&v=4',
+      img: 'https://images.unsplash.com/photo-1657299170932-e75407883a6a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80',
+      text: 'text one',
+      timestamp: '2 hours ago',
+    },
+    {
+      id: '2',
+      name: 'post two',
+      username: 'user two',
+      userImg: 'https://avatars.githubusercontent.com/u/20208332?s=40&v=4',
+      img: 'https://images.unsplash.com/photo-1657299142018-4f7f33aea18c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+      text: 'text two',
+      timestamp: '3 hours ago',
+    },
+  ];
+
   return (
     <div className="xl:ml-[370px] border-l border-r xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl border-gray-200">
       {/* Feed Head */}
@@ -15,6 +37,11 @@ export const FeedComponent: React.FC = () => {
 
       {/* Feed Input */}
       <InputComponent />
+
+      {/* Feed Post */}
+      {posts.map((post) => (
+        <PostComponent key={post.id} post={post} />
+      ))}
     </div>
   );
 };
---
+++ components/Post.tsx
@@ -0,0 +1,68 @@
+import {
+  ChartBarIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  HeartIcon,
+  ShareIcon,
+  TrashIcon,
+} from '@heroicons/react/outline';
+import React from 'react';
+
+interface Props {
+  post: {
+    id: string;
+    name: string;
+    username: string;
+    userImg: string;
+    img: string;
+    text: string;
+    timestamp: string;
+  };
+}
+
+export const PostComponent: React.FC<Props> = ({ post }) => {
+  return (
+    <div className="flex p-3 cursor-pointer border-b" border-gray-200>
+      {/* user image */}
+      <img
+        src={post.userImg}
+        alt="user-img"
+        className="h-11 w-11 rounded-full hover:brightness-95 mr-4"
+      />
+      {/* right part */}
+      <div>
+        {/* header */}
+        <div className="flex items-center justify-between">
+          {/* user info */}
+          <div className="flex items-center selection:space-x-1 whitespace-nowrap">
+            <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
+              {post.name}
+            </h4>
+            <span className="text-sm sm:text-[15px]">@{post.username} - </span>
+            <span className="text-sm sm:text-[15px] hover:underline">
+              {post.timestamp}
+            </span>
+          </div>
+          {/* dot icont */}
+          <DotsHorizontalIcon className="h-10 hover-effects w-10 hover:bg-sky-100 hover:text-sky-500 p-2" />
+        </div>
+
+        {/* post text */}
+        <p className="text-gray-800 text-[15px] sm:text-[16px] mb-2">
+          {post.text}
+        </p>
+        {/* post image */}
+        <img src={post.img} alt="post-img" className="rounded-2xl mr-2" />
+        {/* icons */}
+        <div className="flex items-center justify-between text-gray-500 p-2">
+          <ChatIcon className="h-9 w-9 hover-effects p-2 hover:text-sky-500 hover:bg-sky-100" />
+          <TrashIcon className="h-9 w-9 hover-effects p-2 hover:text-red-500 hover:bg-sky-100" />
+          <HeartIcon className="h-9 w-9 hover-effects p-2 hover:text-red-500 hover:bg-sky-100" />
+          <ShareIcon className="h-9 w-9 hover-effects p-2 hover:text-sky-500 hover:bg-sky-100" />
+          <ChartBarIcon className="h-9 w-9 hover-effects p-2 hover:text-sky-500 hover:bg-sky-100" />
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
