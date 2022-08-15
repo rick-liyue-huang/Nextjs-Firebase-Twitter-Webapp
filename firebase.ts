675705a53e2148d32d26680ab6a06794615ca343
rick-liyue-huang <rick.liyue.huang@gmail.com>
2022-08-15 20:23:52 +1000

    feat: add firebase auth and next-auth

--- firebase.ts
+++ firebase.ts
@@ -1,17 +1,25 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { getApps, initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDEkhXZy6i17jFDnUl8ekFHz-Hai0rEzi0',
-  authDomain: 'nextjs-twitter-project.firebaseapp.com',
-  projectId: 'nextjs-twitter-project',
-  storageBucket: 'nextjs-twitter-project.appspot.com',
-  messagingSenderId: '251301751949',
-  appId: '1:251301751949:web:0cc17fd3315ef8f4eac6c4',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApps();
+
+const db = getFirestore();
+const storage = getStorage();
+
+export { app, db, storage };
